c936daa28720b69fab6e5fd3bd821597d674935a
Satyam Rathi <satyam.rathi47@gmail.com>
2025-06-09 09:17:24 +0530

    we have the grids

---
+++ script.js
@@ -0,0 +1,14 @@
+const row = 16;
+const columns = 16;
+
+const gridContainer = document.querySelector("#MyGrid");
+
+for(let i = 0; i < row; i++){
+    for(let j = 0; j < columns; j++){
+        const gridItem = document.createElement('div');
+        gridItem.className = 'grid-item';
+        gridItem.textContent = `Cell ${i + 1}-${j + 1}`;
+
+        gridContainer.appendChild(gridItem);
+    }
+}
