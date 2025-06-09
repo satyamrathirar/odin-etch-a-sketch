b66dbc4896db13dc2a5c265c62648a6690bbe1d6
Satyam Rathi <satyam.rathi47@gmail.com>
2025-06-09 09:35:28 +0530

    made the grid responsive, not covers the whole screen

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for(let i = 0; i < row; i++){
     for(let j = 0; j < columns; j++){
         const gridItem = document.createElement('div');
         gridItem.className = 'grid-item';
-        gridItem.textContent = `Cell ${i + 1}-${j + 1}`;
+        //gridItem.textContent = `Cell ${i + 1}-${j + 1}`;
 
         gridContainer.appendChild(gridItem);
     }
